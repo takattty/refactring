6457e71c2b16a684e15229c711d4196198bfe546
takattty <r9r_celtics_t9t@icloud.com>
2023-08-15 12:12:42 +0900

    関数の抽出：項目の料金メソッドの抽出

--- src/main/java/first/example/com/Main.java
+++ src/main/java/first/example/com/Main.java
@@ -1,5 +1,6 @@
 package first.example.com;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
@@ -34,23 +35,7 @@ public class Main {
         for (var perf : invoiceJson.get("performances")) {
             var play = playJson.get(perf.get("playID").asText());
             var audience = perf.get("audience").asInt();
-            int thisAmount = 0;
-
-            switch (play.get("type").asText()) {
-                case "tragedy" -> {
-                    thisAmount = 40000;
-                    if (audience > 30) {
-                        thisAmount += (10000 * (audience - 30));
-                    }
-                }
-                case "comedy" -> {
-                    thisAmount = 30000;
-                    if (audience > 20) {
-                        thisAmount += (300 * audience);
-                    }
-                }
-                default -> throw new Error("unkown type: " + play.get("type"));
-            }
+            int thisAmount = amountFor(audience, play);
 
             // ボリューム特典のポイントを加算
             volumeCredits += Math.max(audience - 30, 0);
@@ -63,5 +48,35 @@ public class Main {
         result += "Amount owed is " + format.format(totalAmount/100) + "\n";
         result += "You earned " + volumeCredits + " credits\n";
         System.out.println(result);
+    }
+
+    /**
+     * 1回の演目に対する料金の計算.
+     * <p>
+     * 処理の中で変更されないため、引数で渡している.<br>
+     * 値を変更するのが今回は1つなので、戻り値にしている.
+     * @param audience 観客数
+     * @param play 演目
+     * @return 演目に対する料金
+     */
+    public static int amountFor(int audience, JsonNode play) {
+        int result = 0;
+        switch (play.get("type").asText()) {
+            case "tragedy" -> {
+                result = 40000;
+                if (audience > 30) {
+                    result += (10000 * (audience - 30));
+                }
+            }
+            case "comedy" -> {
+                result = 30000;
+                if (audience > 20) {
+                    result += (300 * audience);
+                }
+            }
+            default -> throw new Error("unkown type: " + play.get("type"));
+        }
+
+        return result;
     }
 }
